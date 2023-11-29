52baab29d542aaa7696aaeb26d57dfa6c7056320
RehanGeorge <rehan.george@gmail.com>
2023-11-29 19:59:46 +0530

    Added Car class with Methods

--- C#ClassesAndObjects/Program.cs
+++ C#ClassesAndObjects/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car audi = new Car("Audi A4", 250, "blue");
+            audi.Drive();
+            audi.Details();
+
+            Car bmw = new Car("BMW M5", 350);
+            bmw.Drive();
+            bmw.Details();
+
+            Console.WriteLine("Press stop to stop the car");
+            string userInput = Console.ReadLine();
+            if (userInput == "stop")
+            {
+                audi.Stop();
+            }
+            else
+            {
+                audi.Drive();
+            }
         }
     }
 }
