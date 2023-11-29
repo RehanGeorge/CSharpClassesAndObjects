d4d9b3b1efa17796e39847c54db0d3ccb722d220
RehanGeorge <rehan.george@gmail.com>
2023-11-29 22:32:10 +0530

    Added Car and Members classes

---
+++ C#ClassesAndObjects/Car.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ClassesAndObjects
+{
+    internal class Car
+    {
+        // Member Variables
+        private string _name;
+        private int _hp;
+        private string _color;
+        private int _maxSpeed;
+
+        // Properties
+        public string Name { 
+            get { return _name; } // Get Accessor
+            set
+            {
+                if (value == "")
+                {
+                    _name = "DefaultName";
+                }
+                else
+                {
+                    _name = value;
+                }
+            } // Set Accessor
+        }
+
+        public int MaxSpeed { set { _maxSpeed = value; } }
+
+        // Default Constructor
+        public Car()
+        {
+            _name = "Car";
+            _hp = 0;
+            _color = "red";
+        }
+
+        // This is a partial specification constructor
+        public Car(string name, int hp = 0)
+        {
+            _name = name;
+            Console.WriteLine($"{name} was created");
+            _hp = hp;
+            _color = "red";
+        }
+
+        // Full specification constructor
+        public Car(string name, int hp, string color)
+        {
+            _name = name;
+            Console.WriteLine($"{name} was created");
+            _hp = hp;
+            _color = color;
+        }
+
+        // Member Setters
+
+        //public void SetName(string name)
+        //{
+        //    if (name == "")
+        //    {
+        //        _name = "DefaultName";
+        //    }
+        //    else
+        //    {
+        //        _name = name;
+        //    }
+
+        //}
+
+        //// Member Getters
+        //public string GetName()
+        //{
+        //    return _name;
+        //}
+
+        public string GetHp() 
+        {
+            return ($"{_hp.ToString()} horsepower");
+        }
+
+        // Member Methods
+        public void Drive()
+        {
+            Console.WriteLine($"{_name} is driving");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine($"{_name} is stopped");
+        }
+
+        public void Details()
+        {
+            Console.WriteLine($"Name: {_name}, HP: {_hp}, Color: {_color}");
+        }
+    }
+}
---
+++ C#ClassesAndObjects/Members.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_ClassesAndObjects
+{
+    internal class Members
+    {
+        // member - private field
+        private string memberName;
+        private string jobTitle;
+        private int salary = 50000;
+
+        // member - public field
+        public int age;
+
+        public string JobTitle {
+            get
+            {
+                return jobTitle;
+            } set
+            {
+                jobTitle = value;
+            }
+        }
+
+        // public member method - can be called from other classes
+        public void Introducing(bool isFriend)
+        {
+            if (isFriend)
+            {
+                SharingPrivateInfo();
+            }
+            else
+            {
+                Console.WriteLine($"Hi, my name is {memberName}, and my job title is {jobTitle}. I'm {age} years old.");
+            }
+        }
+
+        private void SharingPrivateInfo()
+        {
+            Console.WriteLine($"My salary is {salary}");
+        }
+
+        // member constructor
+        public Members()
+        {
+            age = 30;
+            memberName = "Lucas";
+            salary = 60000;
+            jobTitle = "Developer";
+            Console.WriteLine("Object created");
+        }
+
+        // member - finalizer/destructor
+        ~Members()
+        {
+            // cleanup statements...
+            Console.WriteLine("Deconstruction of Members object");
+            Debug.Write("Destruction of Members object");
+        }
+    }
+}
