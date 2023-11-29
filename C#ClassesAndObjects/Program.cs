6996529fd62a83d57510ded75af90a534ea44c41
RehanGeorge <rehan.george@gmail.com>
2023-11-29 22:31:58 +0530

    Added Car and Members classes

--- C#ClassesAndObjects/Program.cs
+++ C#ClassesAndObjects/Program.cs
@@ -2,10 +2,21 @@
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
+            Car myCar = new Car();
+            //myCar.SetName("What a car");
+            //Console.WriteLine(myCar.GetName());
+            /*
+            myCar.Name = "What a car";
+            Console.WriteLine(myCar.Name);
+            myCar.Details();
+
             Car audi = new Car("Audi A4", 250, "blue");
             audi.Drive();
+            Console.WriteLine(audi.GetHp());
             audi.Details();
 
             Car bmw = new Car("BMW M5", 350);
@@ -22,6 +33,11 @@
             {
                 audi.Drive();
             }
+            */
+
+            Members member1 = new Members();
+            member1.Introducing(true);
+            Console.ReadLine();
         }
     }
 }
